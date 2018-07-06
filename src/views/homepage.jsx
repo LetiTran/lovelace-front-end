1371e9e4ab1658d50cfdd992794074d18a5589e8
LetiTran <leticiastran@gmail.com>
2018-07-06 16:11:04 -0700

    Adds landing and home page components

---
+++ src/views/homepage.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+// import PropTypes from 'prop-types';
+import Paper from "@material-ui/core/Paper";
+
+class Homepage extends Component {
+  render() {
+     return (
+      <Paper>
+        Hello, World
+      </Paper> 
+    );
+  }
+}
+
+export default Homepage;
---
+++ src/views/landing-page.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+// import PropTypes from 'prop-types';
+import Login from "../components/login.js"
+
+
+class LandingPage extends Component {
+   
+      render() {
+        return (
+      <Login />
+     
+    );
+  }
+}
+
+export default LandingPage;
