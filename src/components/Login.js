bf922e022bba919ce3cc20c62deaf17ca82bdda4
Kirsten Schumy <kirsten.schumy@gmail.com>
2018-07-23 14:56:54 -0700

    added back Footer.js and Login.js that were removed and readded to make sure names changed

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,13 +1,19 @@
 import React, { Component } from 'react';
 // import PropTypes from 'prop-types';
 import Button from '@material-ui/core/Button';
+import GitHubLogin from 'react-github-login';
 
 class Login extends Component {
 
   render() {
 
     const Welcome = (
-      <Button size="small">Please Login!</Button> 
+        <GitHubLogin
+            clientId='89f6bb7fff6f6582eb54'
+            onSuccess = { response => console.log(response) }
+            onFailure = { response => console.error(response) }
+            redirectUri='http://localhost:3001/'
+        />
     )
 
     return (
